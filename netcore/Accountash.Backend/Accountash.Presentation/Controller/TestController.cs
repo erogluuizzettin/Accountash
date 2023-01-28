6c1b740669b8acf8431a1040d9800e466d42a1ce
izzettin eroğlu <izzettin.eroglu@overtech.com.tr>
2023-01-29 00:39:35 +0300

    Create infrastructure of the accounting project.

---
+++ netcore/Accountash.Backend/Accountash.Presentation/Controller/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Accountash.Presentation.BaseController;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accountash.Presentation.Controller
+{
+    public sealed class TestController: ApiController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Başarılı test.");
+        }
+    }
+}
